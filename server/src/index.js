b34710e1fdbd461b534b96aa962d73b1afcc880a
Yang Xiang <yang.xiang@choice.se>
2020-06-02 14:21:07 +0200

    put middlewares in a separate file

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -17,21 +19,9 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
+app.use(middlewares.notFound);
 
-// eslint-disable-next-line
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? 'ah-oh' : error.stack,
-    });
-});
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
